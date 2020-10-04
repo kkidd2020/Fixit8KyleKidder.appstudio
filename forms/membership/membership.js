05b429c1b7f91e2153cc49be9f0f849396ac5bd4
kkidd2020 <70182581+kkidd2020@users.noreply.github.com>
2020-10-04 15:58:31 -0500

    Initial commit

---
+++ forms/membership/membership.js
@@ -0,0 +1,14 @@
+
+let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
+
+let userName = ""
+
+btnSubmitMem.onclick=function() {
+  userName = inptUserName.value
+  if (members.includes(userName))
+    lblMessage.textContent = "You are a member"
+  else {
+  lblMessageMem.textContent = "You are not a member"
+  members.push(userName)
+  }
+}
